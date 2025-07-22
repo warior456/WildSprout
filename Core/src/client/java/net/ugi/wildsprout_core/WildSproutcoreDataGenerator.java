67888492269e80d7f51c0bf1e184174198de909b
Matteo_fey <matteo.feyaerts@gmail.Com>
2025-07-22 23:51:55 +0200

    something idk, if this action succeeds a miracle occured

---
+++ Core/src/client/java/net/ugi/wildsprout_core/WildSproutcoreDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ugi.wildsprout_core;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WildSproutcoreDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
